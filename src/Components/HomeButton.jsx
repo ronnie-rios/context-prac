5d131023daf631e7a4ae5e0bd252040730fc4b4c
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-05 13:28:46 -0500

    home button component

--- src/Components/HomeButton.jsx
+++ src/Components/HomeButton.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function HomeButton() {
+    const navigate = useNavigate()
   return (
-    <div>HomeButton</div>
+    <div>
+        <button onClick={() => navigate('/')}>go home</button>
+    </div>
   )
 }
