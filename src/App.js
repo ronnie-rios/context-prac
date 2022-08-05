265547b6aa8da97cd0fee8ce8d29fbdacd7d8802
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-04 21:30:20 -0500

    routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ProductProvider } from './Context/Context';
+import Home from './Pages/Home';
+import Details from './Pages/Details';
+import Checkout from './Pages/Checkout';
+import Confirmation from './Pages/Confirmation';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ProductProvider>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path ='/:id' element={<Details />} />
+          <Route path ='/checkout' element={<Checkout />} />
+          <Route path='/confirmation' element={<Confirmation />} />
+        </Routes>
+      </div>
+    </Router>
+    </ProductProvider>
   );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h2>cat products</h2>
+    </div>
   )
 }
