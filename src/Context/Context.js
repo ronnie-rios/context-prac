462a28c5c997d6ec6c2ec5aea2bcf17ceeec49bb
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-04 21:24:20 -0500

    reducer and context created

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,3 +1,32 @@
 import { createContext } from "react"
 
-const DestContext = createContext()
+const ProductContext = createContext()
+
+const data = [
+    { id: 1, name: "cat food", desc: "great food", price: 10 },
+    { id: 2, name: "cat tree", desc: "great tree", price: 20 },
+    { id: 3, name: "catnip", desc: "smells great", price: 30 },
+    { id: 4, name: "litter", desc: "best litter", price: 40 },
+    { id: 5, name: "luxury bed", desc: "comfy bed", price: 50 }
+]
+
+//create the provider
+export const ProductProvider = ({ children }) => {
+    const initialState = {
+        cart: [],
+        Products: data
+    }
+
+
+    const [state, dispatch] = useReducer(productReducer, initialState);
+
+    return <ProductContext.Provider value= {{
+        //state from the usereducer
+        ...state,
+        dispatch,       
+    }}>
+        {children}
+    </ProductContext.Provider>
+}
+
+export default ProductContext;
---
+++ src/Context/Reducer.js
@@ -0,0 +1,18 @@
+const productReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_PRODUCT':
+            return {
+                ...state,
+                cart: action.payload
+            }
+        case 'CLEAR_CART':
+            return {
+                ...state,
+                cart: []
+            }
+        default:
+            return state
+    }
+};
+
+export default productReducer;
