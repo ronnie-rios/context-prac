89e2c25ccfe735a0f9b885632974bae22fa21174
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-04 21:40:27 -0500

    single product page added

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useContext } from 'react';
+import ProductContext from '../Context/Context';
+import { useParams } from 'react-router-dom';
 
 export default function Details() {
+  const { products, cart } = useContext(ProductContext);
+  const params = useParams();
+  const id = params.id;
+
   return (
-    <div>Details</div>
+    <div>
+      <h3>product details</h3>
+      <h4>Name: {products[+id-1].name}</h4>
+      <p>Desc: {products[+id-1].desc}</p>
+      <p>Price: ${products[+id-1].price}</p>
+      <button>add to cart</button>
+    </div>
   )
 }
