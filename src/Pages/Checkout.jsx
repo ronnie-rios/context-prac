b96bdba2f774db24da763961ef9c4b098aec5ff0
Ronnie Rios <ronniesmail14@gmail.com>
2022-08-05 13:31:17 -0500

    checkout page

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import Cart from '../Components/Cart'
-import Form from '../Components/Form'
+import React, { useContext } from 'react';
+import Cart from '../Components/Cart';
+import Form from '../Components/Form';
+import ProductContext from '../Context/Context';
+import HomeButton from '../Components/HomeButton';
 
 export default function Checkout() {
-  return (
-    <div>
+  const { cart } = useContext(ProductContext);
+
+  if (cart.lengt === 0) {
+    return (
+      <div>
+        <h4>nothing in cart yetr</h4>
+        <HomeButton />
+      </div>
+    )
+  } else {
+    return (
+      <div>
       <h2>checkout</h2>
       <Cart />
       <Form />
     </div>
   )
+}
 }
